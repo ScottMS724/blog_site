8a5e5a4c8894fbe625bb1d2cf9d226fcc5e580a2
scottms724 <scottms724@gmail.com>
2021-07-01 13:51:52 -0400

    add basic urls and views

--- main_blog/views.py
+++ main_blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#index
+def starting_page(request):
+    pass 
+
+def posts(request):
+    pass 
+
+def post_detail(request):
+    pass 
