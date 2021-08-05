68610d84e03c4c3cd279deb57d641b09bcdee9a6
scottms724 <scottms724@gmail.com>
2021-08-05 17:55:48 -0400

    update heroku settings

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['blog_site.herokuapp.com']
+ALLOWED_HOSTS = ['django-blog-24.herokuapp.com']
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
