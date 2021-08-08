da5e623613e9433ab3365b6f8bbae68a771381ef
scottms724 <scottms724@gmail.com>
2021-08-08 12:49:38 -0400

    modify allowed hosts

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -29,9 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("IS_DEVELOPMENT", True)
 
-ALLOWED_HOSTS = [
-  getenv("127.0.0.1", "localhost")
-]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
