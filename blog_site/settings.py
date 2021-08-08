4fbb7bf074d31ab7b51e3f7dd29ac1140a94961a
scottms724 <scottms724@gmail.com>
2021-08-08 12:51:53 -0400

    add heroku path allowed hosts

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv("IS_DEVELOPMENT", True)
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ['fast-stream-19246.herokuapp.com/', '127.0.0.1']
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
